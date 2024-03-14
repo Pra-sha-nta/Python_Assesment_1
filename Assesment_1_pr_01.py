45ce6265232baa211af16966a947e14491ca446f
Prashanta Parajuli <161608112+Pra-sha-nta@users.noreply.github.com>
2024-03-14 13:35:08 +0545

    Create Assesment_1_pr_01.py

---
+++ Assesment_1_pr_01.py
@@ -0,0 +1,19 @@
+#Assesment_1_pr_01.py
+a=int(input("Enter First Number "))
+b=int(input("Enter Second Number "))
+c=int(input('''Enter Choice:
+            1.Add
+            2.Sub
+            3.Mul
+            4.Div\n'''))
+if c==1:
+    print("Sum of", a ,"and", b ,"is", a+b)
+elif c==2:
+    print("Difference of",a,"and",b,"is",a-b)
+elif c==3:
+    print("Product of",a,"and",b, "is",a*b)
+elif c==4:
+    print("Quotient of",a,"and",b,"is",a/b)
+else:
+    print("Invalid Choice")
+
